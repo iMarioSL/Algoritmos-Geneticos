89d1a44a0378fc8f9fab5c6835c84ad9a4fdfd31
iMarioSL <ma.sanchez.lr@gmail.com>
2022-07-12 14:44:36 -0500

    cambios

--- GN-PSO.py
+++ GN-PSO.py
@@ -6,31 +6,34 @@ from mpl_toolkits.mplot3d import Axes3D
 
 def graficar(X_best, Y_best):
     
+    # Funcion de Rosenbrock
     f = lambda x,y: (1-x)**2 + 100*(y-x**2)**2;
-    # Initialize figure 
+    
+    # Inicializamos la figura
     figRos = plt.figure(figsize=(7, 7))
-    #axRos = figRos.gca(projection='3d')
     axRos = figRos.add_subplot(projection='3d')
 
-    # Evaluate function
+    # Evaluamos la función.
     X = np.arange(-2, 2, 0.15)
     Y = np.arange(-1, 3, 0.15)
     X, Y = np.meshgrid(X, Y)
     Z = f(X,Y)
-
+    
+    # Evaluamos la función en los mejores puntos.
     Z_best = f(X_best, Y_best)
     
-    # Plot the surface
-    surf = axRos.plot_surface(X, Y, Z, cmap="magma",
-                        linewidth=0)
+    # Graficamos la superficie
+    surf = axRos.plot_surface(X, Y, Z, cmap="magma", linewidth=0)
     
+    # Graficamos el punto (x,y)
     axRos.scatter(X_best , Y_best, Z_best, color="lime", s=50)
     
+    # Damos un limite al eje Z
     axRos.set_zlim(0, 2000)
     
     figRos.colorbar(surf, shrink=0.5, aspect=10)
-    plt.show()
 
+    plt.show()
 
 # Parametros
 pop = 100
@@ -39,47 +42,47 @@ elit = 2
 cruce = 0.9
 T_mut = 0.2
 generation = 100
-
-
-# Ecuacion de Rosenbrock
-# f(x, y) = (a - x)^2 + b(y - x^2)^2
-# a = 1
-# b = 100
-
 a = 1
 b = 100
 
-
+experimentos = 100
 X_best = np.empty([])
 Y_best = np.empty([])
 
-experimentos = 100
-
 for experimento in range(experimentos):
+    # Creamos las matrices de poblacion y el target.
     M_pop = np.zeros((pop, dim))
     M_target = np.zeros((pop, 1))
 
+    # Rellenamos las matrices con valores aleatorios en una distribución
+    # uniforme entre -2 y 3.
     for i in range(pop):
         for j in range(dim):
             M_pop[i, j] = np.random.uniform(-2, 3)
         M_target[i] = (a - M_pop[i, 0])**2 + b*(M_pop[i, 1] - M_pop[i, 0]**2)**2
-        
+    
+    # Concatenamos las matrices anteriores, de modo que tenemos una matriz de 100x3
     M_ag =  np.hstack((M_pop, M_target))
-
+    
+    # Aqui comenzamos con el GA
+    
     gen = 1
     AG = 2
+    
     while gen <= generation:
         # Ordenamos con respecto a la columna target
         M_ag = M_ag[M_ag[:, 2].argsort()]
+        # Creamos una matriz temporal, la que cada generacion
+        # cambiara sus valores
         M_temp = np.zeros((pop, dim))
 
         # Asignacion de dos primeras lineas
         for i in range(elit):
             for j in range(dim):
                 M_temp[i, j] = M_ag[i, j]
-                
+        
+        # Se realiza la seleccion por combate
         for AG in range(2, pop-1): 
-            # Seleccion por combate
             N1 = np.random.randint(0, 99)
             N2 = np.random.randint(0, 99)        
             if N1 == N2:
@@ -97,7 +100,6 @@ for experimento in range(experimentos):
             Padre2 = M_ag[N2, :dim]
             
             # Crossover
-            # Probabilidad de cruzado
             Cross = np.random.rand()
             if Cross <= cruce:
                 
@@ -108,7 +110,7 @@ for experimento in range(experimentos):
                         Padre1[j] = Padre2[j]
                         Padre2[j] = aux
                 
-                # Si hay Crossover entonces puede haber Mutacion
+                # Si hay Crossover entonces puede haber mutacion
                 Mut = np.random.rand()
                 if Mut < T_mut:
                     Pm = np.random.randint(0, 1)
@@ -118,35 +120,35 @@ for experimento in range(experimentos):
                     elif Cm == 1:
                         Padre2[Pm] = np.random.uniform(-2, 3)
             
+            # Cargamos a la matriz temporal con el valor de los padres nuevos
             M_temp[AG] = Padre1
             M_temp[AG + 1] = Padre2
         
+        # Ahora nuestra matriz target se rellana con los valores cargados en la matriz temporal
         for i in range(pop):
             M_target[i] = (a - M_temp[i, 0])**2 + b*(M_temp[i, 1] - M_temp[i, 0]**2)**2
         
         M_ag = np.hstack((M_temp, M_target))
-        
-        #print(f'Generacion: {gen}')
         gen += 1
 
-    # Ordenamos
+    # Ordenamos el valor de la matriz resultado tras pasar 100 generaciones
     M_ag = M_ag[M_ag[:, 2].argsort()]
     
+    # Guardamos los valores x, y encontrados en la corrida
     X_best = np.append(X_best, [M_ag[0, 0]])
     Y_best = np.append(Y_best, [M_ag[0, 1]])
+    
+    # Calculamos el promedio de los valores dados en la lista X_best y Y_best, de 
+    # modo que vemos el valor promedio total obtenido hasta los n numero 
+    # experimentos que hayan transcurrido
     X_best_avg = np.average(X_best)
     Y_best_avg = np.average(Y_best)
 
     print(f"Experimento: {experimento + 1}")
     print(f"X_best: {X_best_avg:.3f}, Y_best: {Y_best_avg:.3f}")
     print("-"*30)
-#print(f'Solucion real: X = 1, Y = 1')
-#print(f'Solucion AG: X = {M_ag[0,0]:.3f}, Y = {M_ag[0,1]:.3f}')
 
-#graficar(M_ag[0,0], M_ag[0,1])
+
 print(f'Solucion real: X = 1, Y = 1')
 print(f'Solucion AG Avg: X = {X_best_avg:.3f}, Y = {Y_best_avg:.3f}')    
 graficar(X_best_avg, Y_best_avg)
-#print(f'Solucion real: X = -9, Y = -10, Z = -8')
-#print(f'Solucion AG Avg: X = {np.average(X)}, Y = {np.average(Y)}, Z = {np.average(Z)}')                
-
